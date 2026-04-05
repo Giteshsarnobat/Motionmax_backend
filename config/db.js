d970c1c448c52ffc0faab64ef69cc4be909ccbb6
Giteshsarnobat <128139962+Giteshsarnobat@users.noreply.github.com>
2026-04-05 20:48:02 +0530

    Add logs for debugging

--- config/db.js
+++ config/db.js
@@ -19,10 +19,10 @@ const pool = mysql.createPool({
   try {
     const conn = await pool.getConnection();
     console.log("✅ MySQL connected successfully");
-    console.log(`DB_HOST: ${DB_HOST}`);
-    console.log(`DB_PORT: ${DB_PORT}`);
-    console.log(`DB_USER: ${DB_USER}`);
-    console.log(`DB_NAME: ${DB_NAME}`);
+    console.log(`DB_HOST: ${process.env.DB_HOST}`);
+    console.log(`DB_PORT: ${process.env.DB_PORT}`);
+    console.log(`DB_USER: ${process.env.DB_USER}`);
+    console.log(`DB_NAME: ${process.env.DB_NAME}`);
     conn.release();
   } catch (err) {
     console.error("❌ MySQL connection failed:", err.message);
